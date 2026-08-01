688f40e4f6eab4ef2e1f8007a75ee696c93a9646
beyondwebco-lgtm <beyondwebco@gmail.com>
2026-08-02 01:07:28 +0530

    Update site photos from new folder

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,6 +14,8 @@ export default function HeroSection() {
     "/images/hero_3.jpg",
     "/images/hero_4.jpg",
     "/images/hero_5.jpg",
+    "/images/tea_banner.jpeg",
+    "/images/spices_combo_poster.jpeg",
   ];
 
   useEffect(() => {
